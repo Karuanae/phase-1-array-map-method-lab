8cc8b6aa4431ce27958015d88d9adea3c9a024f2
karuana <emilykaruanamwangi@gmail.com>
2025-03-12 13:39:42 +0300

    Submit assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
+  'what does the this keyword mean?',//[0]
+  'What is the Constructor OO pattern?',//[1]
+  'implementing Blockchain Web API',//[2]
+  'The Test Driven Development Workflow',//[3]
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(element => element.split(" ")
+.map(word => word[0].toUpperCase () + word.slice (1))
+.join (" ")
+);
+ 
+
+// We have 10 indexes
+//.upperCase
+//.string.length
+//index.length
+//
